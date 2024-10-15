5236ff2d1a6aba020b7cb308b526a0c5b10f6613
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-10-15 12:33:35 +0200

    add custom pagination in events

--- src/main/java/no/fintlabs/EventService.java
+++ src/main/java/no/fintlabs/EventService.java
@@ -1,6 +1,7 @@
 package no.fintlabs;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.model.Event;
 import no.fintlabs.model.EventDto;
 import no.fintlabs.model.ManualEventDto;
@@ -20,6 +21,7 @@ import static no.fintlabs.EventNames.INSTANCE_DELETED;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EventService {
 
     private final EventRepository eventRepository;
@@ -33,35 +35,49 @@ public class EventService {
     }
 
     public Page<EventDto> getMergedLatestEvents(Pageable pageable) {
-        List<Event> latestEvents = eventRepository
-                .findLatestEventPerSourceApplicationInstanceId(pageable).getContent();
+        Page<Event> latestEventsPage = eventRepository
+                .findLatestEventPerSourceApplicationInstanceId(pageable);
 
-        List<Event> latestNonDeletedEvents = eventRepository
-                .findLatestEventNotDeletedPerSourceApplicationInstanceId(pageable).getContent();
+        Page<Event> latestNonDeletedEventsPage = eventRepository
+                .findLatestEventNotDeletedPerSourceApplicationInstanceId(pageable);
 
-        return getEventDtos(pageable, latestNonDeletedEvents, latestEvents);
+        long totalLatestEvents = eventRepository.countLatestEventPerSourceApplicationInstanceId();
+        long totalLatestNonDeletedEvents = eventRepository.countLatestEventNotDeletedPerSourceApplicationInstanceId();
+
+        long totalElements = totalLatestEvents + totalLatestNonDeletedEvents;
+
+        List<EventDto> mergedEvents = mergeEvents(latestEventsPage.getContent(), latestNonDeletedEventsPage.getContent());
+
+        return new PageImpl<>(mergedEvents, pageable, totalElements);
     }
 
     public Page<EventDto> getMergedLatestEventsWhereSourceApplicationIdIn(
             List<Long> sourceApplicationIds,
             Pageable pageable
     ) {
-        List<Event> latestEvents = eventRepository
+        Page<Event> latestEventsPage = eventRepository
                 .findLatestEventPerSourceApplicationInstanceIdAndSourceApplicationIdIn(
                         sourceApplicationIds,
                         pageable
-                ).getContent();
+                );
 
-        List<Event> latestNonDeletedEvents = eventRepository
+        Page<Event> latestNonDeletedEventsPage = eventRepository
                 .findLatestEventNotDeletedPerSourceApplicationInstanceIdAndSourceApplicationIdIn(
                         sourceApplicationIds,
                         pageable
-                ).getContent();
+                );
 
-        return getEventDtos(pageable, latestNonDeletedEvents, latestEvents);
+        long totalLatestEvents = eventRepository.countLatestEventPerSourceApplicationInstanceIdAndSourceApplicationIdIn(sourceApplicationIds);
+        long totalLatestNonDeletedEvents = eventRepository.countLatestEventNotDeletedPerSourceApplicationInstanceIdAndSourceApplicationIdIn(sourceApplicationIds);
+
+        long totalElements = totalLatestEvents + totalLatestNonDeletedEvents;
+
+        List<EventDto> mergedEvents = mergeEvents(latestEventsPage.getContent(), latestNonDeletedEventsPage.getContent());
+
+        return new PageImpl<>(mergedEvents, pageable, totalElements);
     }
 
-    private PageImpl<EventDto> getEventDtos(Pageable pageable, List<Event> latestNonDeletedEvents, List<Event> latestEvents) {
+    private List<EventDto> mergeEvents(List<Event> latestEvents, List<Event> latestNonDeletedEvents) {
         Map<String, Event> nonDeletedEventMap = latestNonDeletedEvents.stream()
                 .collect(
                         Collectors.toMap(
@@ -87,13 +103,69 @@ public class EventService {
             }
         }
 
-        int start = (int) pageable.getOffset();
-        int end = Math.min((start + pageable.getPageSize()), mergedEvents.size());
-
-        List<EventDto> paginatedList = mergedEvents.subList(start, end);
-
-        return new PageImpl<>(paginatedList, pageable, mergedEvents.size());
+        return mergedEvents;
     }
+
+//    public Page<EventDto> getMergedLatestEventsWhereSourceApplicationIdIn(
+//            List<Long> sourceApplicationIds,
+//            Pageable pageable
+//    ) {
+//        List<Event> latestEvents = eventRepository
+//                .findLatestEventPerSourceApplicationInstanceIdAndSourceApplicationIdIn(
+//                        sourceApplicationIds,
+//                        pageable
+//                ).getContent();
+//
+//        List<Event> latestNonDeletedEvents = eventRepository
+//                .findLatestEventNotDeletedPerSourceApplicationInstanceIdAndSourceApplicationIdIn(
+//                        sourceApplicationIds,
+//                        pageable
+//                ).getContent();
+//
+//        return getEventDtos(pageable, latestNonDeletedEvents, latestEvents);
+//    }
+
+//    private PageImpl<EventDto> getEventDtos(Pageable pageable, List<Event> latestNonDeletedEvents, List<Event> latestEvents) {
+//
+//        log.info("Latest Events Size: " + latestEvents.size());
+//        log.info("Latest Non-Deleted Events Size: " + latestNonDeletedEvents.size());
+//
+//
+//        Map<String, Event> nonDeletedEventMap = latestNonDeletedEvents.stream()
+//                .collect(
+//                        Collectors.toMap(
+//                                event -> event.getInstanceFlowHeaders().getSourceApplicationInstanceId(),
+//                                event -> event
+//                        )
+//                );
+//
+//        List<EventDto> mergedEvents = new ArrayList<>();
+//
+//        for (Event latestEvent : latestEvents) {
+//            if (INSTANCE_DELETED.equals(latestEvent.getName())) {
+//                Event nonDeletedEvent = nonDeletedEventMap
+//                        .get(latestEvent.getInstanceFlowHeaders().getSourceApplicationInstanceId());
+//                if (nonDeletedEvent != null) {
+//                    EventDto updatedEventDto = EventToEventDto(nonDeletedEvent);
+//                    updatedEventDto.setStatus(INSTANCE_DELETED);
+//                    mergedEvents.add(updatedEventDto);
+//                }
+//            } else {
+//                EventDto eventDto = EventToEventDto(latestEvent);
+//                mergedEvents.add(eventDto);
+//            }
+//        }
+//
+//        log.info("Merged Events Size: " + mergedEvents.size());
+//
+//
+//        int start = (int) pageable.getOffset();
+//        int end = Math.min((start + pageable.getPageSize()), mergedEvents.size());
+//
+//        List<EventDto> paginatedList = mergedEvents.subList(start, end);
+//
+//        return new PageImpl<>(paginatedList, pageable, mergedEvents.size());
+//    }
 
     public Optional<Event> findFirstByInstanceFlowHeadersSourceApplicationIdAndInstanceFlowHeadersSourceApplicationInstanceIdAndInstanceFlowHeadersSourceApplicationIntegrationIdOrderByTimestampDesc(
             ManualEventDto manualEventDto
