2cd09d9b0750441b18d0636fadc9c3621ecba6bd
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-05-22 13:58:20 +0200

    add retention time in ms property for instance topics

--- src/main/java/no/fintlabs/kafka/InstanceReceivalErrorConfiguration.java
+++ src/main/java/no/fintlabs/kafka/InstanceReceivalErrorConfiguration.java
@@ -5,6 +5,8 @@ import no.fintlabs.kafka.event.error.topic.ErrorEventTopicService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import static no.fintlabs.model.event.EventCategory.INSTANCE_RECEIVAL_ERROR;
+
 @Configuration
 public class InstanceReceivalErrorConfiguration {
 
@@ -13,7 +15,7 @@ public class InstanceReceivalErrorConfiguration {
             @Value("${fint.kafka.topic.instance-retention-ms}") long retentionMs
     ) {
         ErrorEventTopicNameParameters instanceProcessingErrorTopicNameParameters = ErrorEventTopicNameParameters.builder()
-                .errorEventName("instance-receival-error")
+                .errorEventName(INSTANCE_RECEIVAL_ERROR.getEventName())
                 .build();
 
         errorEventTopicService.ensureTopic(instanceProcessingErrorTopicNameParameters, retentionMs);
