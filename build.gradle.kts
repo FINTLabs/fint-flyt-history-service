8b3f856c2b86f8e6d0a8bd3586d3781c057de8cc
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-06-26 14:10:10 +0200

    chore: bump flyt-audit-starter til 1.0.0-rc-7

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
 
     implementation("no.novari:flyt-web-resource-server:3.1.0")
     implementation("no.novari:flyt-kafka:7.0.0")
-    implementation("no.novari:flyt-audit-starter:1.0.0-rc-6")
+    implementation("no.novari:flyt-audit-starter:1.0.0-rc-7")
 
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
