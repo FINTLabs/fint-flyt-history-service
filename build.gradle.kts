d447eafde15ead45094da6c7d8448bc51b5ce675
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-21 09:03:34 +0200

    fix: bump jackson, log4j2, netty og commons-compress

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,8 +47,8 @@ repositories {
 }
 
 extra["commons-lang3.version"] = "3.18.0"
-extra["jackson-bom.version"] = "2.21.5"
-extra["log4j2.version"] = "2.25.5"
+extra["jackson-bom.version"] = "2.22.2"
+extra["log4j2.version"] = "2.26.1"
 extra["postgresql.version"] = "42.7.12"
 
 dependencies {
